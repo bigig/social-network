30e37aad1c5cbbaa3d239a5f5e3ea312ac9b0939
Ihar <iharthrush@gmail.com>
2014-05-09 00:47:43 +0300

    Let users edit their profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to current_user, notice: "Your profile updated successfully"
+    else
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
