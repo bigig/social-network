a7fe960e008661de9ea5d2092c816e11c2c5e08b
Ihar <iharthrush@gmail.com>
2014-05-09 00:49:04 +0300

    Let users upload their avatars on Dropbox

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar,
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   has_secure_password
 
-  attr_accessible :about, :birthday, :email, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible :about, :birthday, :email, :first_name, :last_name, :password, :password_confirmation, :avatar
 
   validates_uniqueness_of :email
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140407152821) do
+ActiveRecord::Schema.define(:version => 20140508210755) do
 
   create_table "messages", :force => true do |t|
     t.string   "user_id"
@@ -28,8 +28,12 @@ ActiveRecord::Schema.define(:version => 20140407152821) do
     t.string   "last_name"
     t.datetime "birthday"
     t.string   "about"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
