9e3c0b1b05eb4d332370b553488fbba68fc41f48
Ihar <iharthrush@gmail.com>
2014-05-09 00:48:20 +0300

    Adding some formatting to users profiles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,18 @@
 module UsersHelper
+
+  def pretty field_label, field
+    content_tag :div,
+    content_tag(:span, whom(field_label), class: 'whom') + ' ' +
+      content_tag(:span, field, class: 'actually-field')
+  end
+
+  private
+
+  def whom field
+    if current_user == @user
+      "Your #{field}:"
+    else
+      "His #{field}:"
+    end
+  end
 end
